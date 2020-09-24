19b5dd3442f8b3c95f401d7e65aa3efb608ccab0
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2020-09-24 12:18:14 +0530

    fourth commit

--- UC1.sh
+++ UC1.sh
@@ -1,9 +1,25 @@
 #! /bin/bash -x
 
 
-isFullTime=8
+isFullTime=2
 empPerHr=20
-isPartTime=8
+isPartTime=1
 
 #use RANDOM function to check for attendance 
 empcheck=$((RANDOM%3))
+
+
+#use use case 
+
+case $empCheck in
+
+	1) $isFullTime 
+		empHrs= 8
+		;;
+	2) $isParttime
+		empHrs=4
+		;;
+	*)
+		empHrs=0
+		;;
+esac
